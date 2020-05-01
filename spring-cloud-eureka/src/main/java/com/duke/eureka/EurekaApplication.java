bb098c22a1ec04db0332b162285d57c6d4beadc6
WeakyDuke <971395616@qq.com>
2020-05-01 11:28:13 +0800

    eureka修改多节点配置

--- spring-cloud-eureka/src/main/java/com/duke/eureka/EurekaApplication.java
+++ spring-cloud-eureka/src/main/java/com/duke/eureka/EurekaApplication.java
@@ -2,10 +2,11 @@ package com.duke.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication(scanBasePackages = "com")
+@SpringBootApplication(scanBasePackages = "com",exclude = { DataSourceAutoConfiguration.class })
 public class EurekaApplication {
 
     public static void main(String[] args) {
--- spring-cloud-eureka/src/main/java/com/duke/hello/HelloController.java
+++ spring-cloud-eureka/src/main/java/com/duke/hello/HelloController.java
@@ -1,6 +1,5 @@
 package com.duke.hello;
 
-import com.duke.eureka.DukeAspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,6 @@ public class HelloController {
 
     private static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    //@DukeAspect
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         aspectTest("duke");
@@ -28,8 +26,7 @@ public class HelloController {
         logger.info("hello the world");
         return "Hello World";
     }
-
-    //@DukeAspect
+    
     public String aspectTest(String name){
         logger.info("aspectTest: {}", name);
         return name;
